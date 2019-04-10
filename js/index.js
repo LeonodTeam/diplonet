7bf7216bcdcd4a54dbe90ef8e4c52a6721dc3acb
kaio <elmanuem@gmail.com>
2019-04-10 18:24:58 +0200

    Added about page and verify page

--- js/index.js
+++ js/index.js
@@ -14,20 +14,59 @@ function set_active(element) {
 function show_home() {
     const homepage_div = document.getElementById('home_div');
     const certify_div = document.getElementById('certif_diplom_div');
+    const verify_div = document.getElementById('verif_diplom_div');
+    const aboutpage_div = document.getElementById('about_div');
     homepage_div.style.display = 'block';
     certify_div.style.display = 'none';
-    set_active(document.getElementById('header_link_home'))
+    verify_div.style.display = 'none';
+    aboutpage_div.style.display = 'none';
+    // Set the header link as active
+    set_active(document.getElementById('header_link_home'));
 }
 
-// Shows the page to certify a diplom
+// Shows the page to certify a diploma
 // VOID
 function show_certify() {
     const homepage_div = document.getElementById('home_div');
     const certify_div = document.getElementById('certif_diplom_div');
+    const verify_div = document.getElementById('verif_diplom_div');
+    const aboutpage_div = document.getElementById('about_div');
     homepage_div.style.display = 'none';
     certify_div.style.display = 'block';
+    verify_div.style.display = 'none';
+    aboutpage_div.style.display = 'none';
     // Set the header link as active
     set_active(document.getElementById('header_link_certify'));
+}
+
+// Shows the page to verify a diploma
+// VOID
+function show_verify() {
+    const homepage_div = document.getElementById('home_div');
+    const certify_div = document.getElementById('certif_diplom_div');
+    const verify_div = document.getElementById('verif_diplom_div');
+    const aboutpage_div = document.getElementById('about_div');
+    homepage_div.style.display = 'none';
+    certify_div.style.display = 'none';
+    verify_div.style.display = 'block';
+    aboutpage_div.style.display = 'none';
+    // Set the header link as active
+    set_active(document.getElementById('header_link_verify'));
+}
+
+// Shows the page about
+// VOID
+function show_about() {
+    const homepage_div = document.getElementById('home_div');
+    const certify_div = document.getElementById('certif_diplom_div');
+    const verify_div = document.getElementById('verif_diplom_div');
+    const aboutpage_div = document.getElementById('about_div');
+    homepage_div.style.display = 'none';
+    certify_div.style.display = 'none';
+    verify_div.style.display = 'none';
+    aboutpage_div.style.display = 'block';
+    // Set the header link as active
+    set_active(document.getElementById('header_link_about'));
 }
 
 // Checks entries from user in the form to certify a diplom
@@ -202,10 +241,15 @@ function setup() {
         // ======== Events ===========
         // Homepage
         document.getElementById('homepage_certify_button').addEventListener('click', show_certify, false);
+        document.getElementById('homepage_verify_button').addEventListener('click', show_verify, false);
+        document.getElementById('homepage_graduate_button').addEventListener('click', show_verify, false);
         // Header
         document.getElementById('header_logo').addEventListener('click', show_home, false);
         document.getElementById('header_link_home').addEventListener('click', show_home, false);
         document.getElementById('header_link_certify').addEventListener('click', show_certify, false);
+        document.getElementById('header_link_verify').addEventListener('click', show_verify, false);
+        document.getElementById('header_link_about').addEventListener('click', show_about, false);
+
         // Certify page
         document.getElementById('btn_certify').addEventListener('click', (e) => {
             e.preventDefault();
